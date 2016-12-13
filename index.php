dbfac11910d6aed4d3c06b3301a67413c73b6825
ashwongg <ashwong1994@gmail.com>
2016-12-12 16:57:19 -0800

    uploaded Project

---
+++ index.php
@@ -0,0 +1,103 @@
+	<!DOCTYPE html>
+	<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Medical Feedback</title>
+
+		<!-- Bootstrap Core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Custom CSS -->
+		<link href="css/simple-sidebar.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+
+	</head>
+
+	<body>
+
+		<div id="wrapper">
+
+			<!-- Sidebar -->
+			<div id="sidebar-wrapper">
+				<ul class="sidebar-nav">
+					<li class="sidebar-brand">
+						<a href="index.php">
+							Medical Feedback
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- /#sidebar-wrapper -->
+
+			<!-- Page Content -->
+			<div id="page-content-wrapper">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-lg-12">
+							<?php $name= ""?> 
+							<?php $lvl=""?>
+							<h2> Medical Feedback </h2> 
+
+							<br>
+							<h4> This is a Learning Analytics (COSC 419C) Project with a focus on taking a user's <br>
+								 level of understanding and providing the information according to the their level. </h4> 
+							<br>
+							<h4> Choose a disease and condition from here: <a href="http://www.mayoclinic.org/diseases-conditions/index">Diseases and Conditions</a></h4>
+
+							<form method="get" action="result.php">
+								<h3>Disease or Condition: <input type="text" name="name"><br>
+								<br>
+								Level of Understanding:<br>
+								<br> Basic: <input type="radio" name="lvl" value="Basic"> <br>
+								Expert: <input type="radio" name="lvl" value="Expert"> </h3>
+								<br>
+								<br>
+								<input type="submit" value="Submit">
+							</form>
+							<br>
+							<h4> Information provided by MayoClinic.org</h4> 
+							<br>
+							<h5> Created by: Ashley Wong (32297129) </h5> 
+
+
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /#page-content-wrapper -->
+
+			</div>
+			<!-- /#wrapper -->
+
+			<!-- jQuery -->
+			<script src="js/jquery.js"></script>
+
+			<!-- Bootstrap Core JavaScript -->
+			<script src="js/bootstrap.min.js"></script>
+
+			<!-- Menu Toggle Script -->
+			<script>
+			$("#menu-toggle").click(function(e) {
+				e.preventDefault();
+				$("#wrapper").toggleClass("toggled");
+			});
+			</script>
+
+
+
+		</body>
+
+		</html>
---
+++ result.php
@@ -0,0 +1,81 @@
+	<!DOCTYPE html>
+	<html lang="en">
+
+	<head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<title>Medical Feedback</title>
+
+		<!-- Bootstrap Core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+
+		<!-- Custom CSS -->
+		<link href="css/simple-sidebar.css" rel="stylesheet">
+
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+
+	</head>
+
+	<body> 
+		<body>
+
+			<div id="wrapper">
+
+				<!-- Sidebar -->
+				<div id="sidebar-wrapper">
+					<ul class="sidebar-nav">
+						<li class="sidebar-brand">
+							<a href="index.php">
+								Medical Feedback
+							</a>
+						</li>
+	
+
+					</ul>
+				</div>
+				<!-- /#sidebar-wrapper -->
+
+				<!-- Page Content -->
+				<div id="page-content-wrapper">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-lg-12">
+								<?php
+								$name = $_GET["name"]; 
+								echo "<h2> $name </h2>";
+								$string = file_get_contents("data.json");
+								$json_a = json_decode($string,true);
+
+								foreach ($json_a as $med_name => $med_a)
+									if (strcasecmp($_GET["name"],$med_a['title'])==0){
+										$med_a['text'] = str_replace("[By Mayo Clinic Staff](http://www.mayoclinic.org/about-this-site/welcome)","",$med_a['text']);
+										if (strcasecmp($_GET["lvl"], "Basic")==0){
+											$med_a['text'] = str_replace("\n\n","<br><br>",$med_a['text']);
+											$arr= explode('##',$med_a['text']);
+											echo $arr[1];
+											#echo $med_a['text'];
+										} else {
+											$med_a['text'] = str_replace("[By Mayo Clinic Staff](http://www.mayoclinic.org/about-this-site/welcome)","",$med_a['text']);
+											$med_a['text'] = str_replace("#","",$med_a['text']);
+											$med_a['text'] = str_replace("*","",$med_a['text']);
+											$med_a['text'] = str_replace("\n\n","<br><br>",$med_a['text']);
+											echo $med_a['text'];
+										}
+									}
+
+
+									?>
+								</div>
+							</div>
+						</div>
+					</div>
